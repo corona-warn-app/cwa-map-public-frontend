16afde87a18a546be30cbb84b9c07e734af962dc
Dirk Reske <dirk.reske@t-systems.com>
2021-11-24 11:41:27 +0100

    Fixed some style issues

--- webpack.config.js
+++ webpack.config.js
@@ -43,7 +43,7 @@ module.exports = {
     devServer: {
         contentBase: path.join(__dirname, 'dist'),
         compress: false,
-        port: 9000,
+        port: 9090,
         watchContentBase: true,
         liveReload: true,
         proxy: {
